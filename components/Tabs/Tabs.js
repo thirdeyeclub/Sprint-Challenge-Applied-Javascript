16896c1b643be7297b8731e93e6b33c97628525e
thirdeyeclub <wwwaaavvveeerrr@gmail.com>
2019-02-01 12:10:48 -0500

    added Event Listener, fixed some issues wth query selectors and worte the array.from map fuction

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -11,18 +11,18 @@ class TabLink {
     // Check to see if this.tabData is equal to 'all'
     if(this.tabData = 'all'){
       // If `all` is true, select all cards regardless of their data attribute values
-      this.cards = document.querySelector('.card');
+      this.cards = document.querySelectorAll('.card');
     } else {
       // else if `all` is false, only select the cards with matching this.tabData values
-      this.cards = document.querySelector(`.cards[data-tab='${this.tabData}']`);
+      this.cards = document.querySelectorAll(`.cards[data-tab='${this.tabData}']`);
     }
     
 
      // Map over the newly converted NodeList we just created in our if statement above. Convert each this.cards element into a new instance of the TabCard class. Pass in a card object to the TabCard class. 
-    // this.cards = Array.from(this.cards).map();
+    this.cards = Array.from(this.cards).map(card => new TabCard(card) );
 
     // Add a click event that invokes this.selectTab
-    // this.tabElement.addEventListener();
+    this.tabElement.addEventListener('click' => this.selectTab );
   }
 
   selectTab(){
